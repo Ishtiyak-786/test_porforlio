1ca6bcf12775b86d1502f54bae58d11ede2e9c54
Ishtiyak-786 <103363694+Ishtiyak-786@users.noreply.github.com>
2023-06-05 22:47:14 +0530

    Initial commit

---
+++ resume_temp.js
@@ -0,0 +1,5 @@
+// const address = document.getElementById("address");
+// address.style.color = "#121e8a";
+const locations=()=>{
+    const loc=<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5426.141423456679!2d72.84522359235154!3d19.164178139739132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b652ec3d091f%3A0xe6f050a1c35b88f2!2sGoregaon!5e0!3m2!1sen!2sin!4v1685981637454!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+}
